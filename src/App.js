df69b65343abd92a4ca4d5c1ef5caa2fad5c43b6
Sercan Paspal <sercanpaspal@gmail.com>
2021-12-10 23:49:09 +0300

    frontend socket connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const socket = io(process.env.REACT_APP_SOCKET_URI, {
+  transports: ["websocket"],
+});
+
+const App = () => {
+  return <div className="App">hello</div>;
+};
 
 export default App;
