03663143347f8a024850f9f01cdab653284f4671
Sercan Paspal <sercanpaspal@gmail.com>
2021-12-12 21:33:56 +0300

    github corner added

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,5 @@
 import ThemeList from "../ThemeList";
+import GithubCorner from "react-github-corner";
 import styles from "./index.module.css";
 
 const Layout = ({ children }) => {
@@ -6,6 +7,14 @@ const Layout = ({ children }) => {
     <div className={styles.layout}>
       <ThemeList />
       {children}
+
+      <GithubCorner
+        href="https://github.com/sercanpaspal/potter-maze"
+        bannerColor="var(--primary-500)"
+        octoColor="var(--secondary-500)"
+        size="80"
+        direction="right"
+      />
     </div>
   );
 };
