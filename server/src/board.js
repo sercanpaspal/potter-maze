e8aaf13395b324552fd448a3510346014fb3d3e4
Sercan Paspal <sercanpaspal@gmail.com>
2021-12-12 10:06:11 +0300

    road drawed

--- server/src/board.js
+++ server/src/board.js
@@ -23,4 +23,124 @@ module.exports = [
     type: "card",
     coord: [5, 4],
   },
+  {
+    type: "road",
+    coord: [6, 4],
+  },
+  {
+    type: "road",
+    coord: [7, 4],
+  },
+  {
+    type: "road",
+    coord: [7, 3],
+  },
+  {
+    type: "road",
+    coord: [7, 2],
+  },
+  {
+    type: "road",
+    coord: [7, 1],
+  },
+  {
+    type: "road",
+    coord: [7, 0],
+  },
+  {
+    type: "road",
+    coord: [6, 0],
+  },
+  {
+    type: "road",
+    coord: [5, 0],
+  },
+  {
+    type: "road",
+    coord: [5, 1],
+  },
+  {
+    type: "road",
+    coord: [5, 2],
+  },
+  {
+    type: "road",
+    coord: [4, 2],
+  },
+  {
+    type: "road",
+    coord: [3, 2],
+  },
+  {
+    type: "road",
+    coord: [3, 1],
+  },
+  {
+    type: "road",
+    coord: [3, 0],
+  },
+  {
+    type: "road",
+    coord: [2, 0],
+  },
+  {
+    type: "road",
+    coord: [1, 0],
+  },
+  {
+    type: "road",
+    coord: [0, 0],
+  },
+  {
+    type: "road",
+    coord: [0, 1],
+  },
+  {
+    type: "road",
+    coord: [0, 2],
+  },
+  {
+    type: "road",
+    coord: [1, 2],
+  },
+  {
+    type: "road",
+    coord: [1, 3],
+  },
+  {
+    type: "road",
+    coord: [1, 4],
+  },
+  {
+    type: "road",
+    coord: [2, 4],
+  },
+  {
+    type: "road",
+    coord: [3, 4],
+  },
+  {
+    type: "road",
+    coord: [3, 5],
+  },
+  {
+    type: "road",
+    coord: [3, 6],
+  },
+  {
+    type: "road",
+    coord: [3, 7],
+  },
+  {
+    type: "road",
+    coord: [2, 7],
+  },
+  {
+    type: "road",
+    coord: [1, 7],
+  },
+  {
+    type: "road",
+    coord: [0, 7],
+  },
 ];
