b42df5fb9366e72aae3fd15543a6ca850327c5bb
Sercan Paspal <sercanpaspal@gmail.com>
2021-12-12 21:27:37 +0300

    end screen surprize :rocket:

--- src/scenes/GameEnd/index.js
+++ src/scenes/GameEnd/index.js
@@ -1,18 +1,21 @@
 import { connect } from "react-redux";
 import ButtonReturnMenu from "../../components/ButtonReturnMenu";
 import RoomUserList from "../../components/RoomUserList";
+import styles from "./index.module.css";
+import cn from "classnames";
 
 const GameEnd = ({ game, user }) => {
-  const {
-    winnerUser: { id, name, figure },
-  } = game;
+  const { winnerUser } = game;
+
+  const isWinner = winnerUser?.id === user.id;
 
   return (
     <div>
+      <div className={cn([styles.bg, { [styles.winner]: isWinner }])}></div>
       <h1>
         kazanan{" "}
         <u>
-          {figure} {name} {id === user.id && "(sen)"}
+          {winnerUser?.figure} {winnerUser?.name} {isWinner && "(sen)"}
         </u>
         !
       </h1>
